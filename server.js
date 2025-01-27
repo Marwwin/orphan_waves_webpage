3d48f8f5aa0b2df14b68c1beba3e8f759ade5ecd
Marwwin <markus.kankkonen@protonmail.com>
2025-01-27 22:18:41 +0200

    fixed album info

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const albums = [
     name: "Keel",
     year: 2024,
     album_cover: "keel_small.jpeg",
+    mixed_by: "Orphan Waves",
+    recorded_by: "Joonatan Turkki",
+    recorded_at: "Magnusborg Studios",
+    mastered_by: "Orphan Waves",
+    cover_art: "Riku Lekman",
     spotify_link: "https://open.spotify.com/album/5NI1GQeK5u2FTIEM4N3jZn",
     youtube_link:
       "https://www.youtube.com/playlist?list=OLAK5uy_lRYRwp44y8OECnYgbsVEyQaL_I_fiSndE",
@@ -21,6 +26,12 @@ const albums = [
     name: "Persona",
     year: 2023,
     album_cover: "persona_small.jpeg",
+    mixed_by: "Orphan Waves",
+    recorded_at: "Hollywood House Studios",
+    recorded_by: "Matias Kiiveri",
+    mastered_by: "Robin Sutherland",
+    mastered_at: "Esko Mastering",
+    cover_art: "Toimi Tytti",
     spotify_link:
       "https://open.spotify.com/album/6QW6mXroON0hVTL11Cs1W3?si=Ral12uJJSRm6-NhLUU7G4Q",
     youtube_link:
@@ -36,6 +47,12 @@ const albums = [
     name: "Caveat",
     year: 2022,
     album_cover: "caveat_small.jpeg",
+    mixed_by: "Orphan Waves",
+    recorded_at: "Soundwell Studios",
+    recorded_by: "Valtteri Kallio",
+    mastered_by: "Robin Sutherland",
+    mastered_at: "Esko Mastering",
+    cover_art: "Dall-e",
     spotify_link:
       "https://open.spotify.com/album/6QW6mXroON0hVTL11Cs1W3?si=Ral12uJJSRm6-NhLUU7G4Q",
     youtube_link:
@@ -50,6 +67,12 @@ const albums = [
   {
     name: "Soirée",
     album_cover: "soiree_small.jpeg",
+    mixed_by: "Orphan Waves",
+    recorded_at: "Altai Studios",
+    recorded_by: "Tuomas Paavola",
+    mastered_by: "Robin Sutherland",
+    mastered_at: "Esko Mastering",
+    cover_art: "Leonor Ruiz Dubrovin",
     year: 2021,
     spotify_link: "https://open.spotify.com/album/2yEG4NOFZye0sfTNIcNtrv",
     youtube_link:
@@ -213,45 +236,55 @@ function createAlbum(index) {
           <div class="arrow left"
             hx-get="/album/${index - 1}"
             hx-target="closest section"
-            hx-swap="outerHTML"
+            hx-swap="outerHTML swap:500ms"
       >&lt;</div> `
           : ""
-      }
+       }
       <div id="album_container">
-      <h2>Albums</h2>
         <div id="album_cover">
-          <img alt="Album cover" src="./assets/${album.album_cover}" />
+          <img id="album_cover_img" alt="Album cover" src="./assets/${album.album_cover}" />
           <div id="album_info">
-            <h3>${album.name}</h3>
+            <h2>${album.name}</h2>
+
+            <div>
+              <h3>Tracks:</h3>
+              <ol>
+                ${album.tracks.map((track) => `<li>${track.name}</li>`).join("")}
+              </ol>
+            </div>
+          <div id="album_personel">
             <ul>
-              ${album.tracks.map((track) => `<li>${track.name}</li>`).join('')}
+            <li>Released: ${album.year}
+            <li>Recorded by: ${album.recorded_by} @ ${album.recorded_at}</li>
+            <li>Mixed by: ${album.mixed_by}</li>
+            <li>Mastered by: ${album.mastered_by} ${album.mastered_at ? `@ ${album.mastered_at}` : ""} </li>
+            <li>Cover art by: ${album.cover_art}</li>
             </ul>
+            </div>
+        <div id="album_links">
+          <a href="${album.spotify_link}" >
+            <img
+              id="spotify_logo"
+              alt="Spotify logo"
+              src="./assets/Spotify_Logo_CMYK_White.png"
+          /></a>
+          <a href="${album.youtube_link}" >
+            <img
+              id="youtube_logo"
+              alt="Youtube logo"
+              src="assets/yt_logo_mono_dark.png"
+          /></a>
           </div>
-        </div>      <div id="album_links">
-              <a href="${album.spotify_link}" >
-                <img
-                  id="spotify_logo"
-                  alt="Spotify logo"
-                  src="./assets/Spotify_Logo_CMYK_White.png"
-              /></a>
-              <a
-                href="${album.youtube_link}"
-              >
-                <img
-                  id="youtube_logo"
-                  alt="Youtube logo"
-                  src="assets/yt_logo_mono_dark.png"
-              /></a>
-            </div>
-            </div>
+          </div>
+        </div>
+        </div>
       ${
         index < limit
           ? `
           <div class="arrow right"
             hx-get="/album/${index + 1}"
             hx-target="closest section"
-            hx-swap="outerHTML swap:300ms"
-
+            hx-swap="outerHTML swap:500ms"
       >&gt;</div> `
           : ""
       }
